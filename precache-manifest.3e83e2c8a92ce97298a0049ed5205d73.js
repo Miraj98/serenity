425a7902c67ca882114438a52f0db6489131b1eb
Miraj <mirajholmes@gmail.com>
2019-07-19 21:12:24 +0530

    Updates

---
+++ precache-manifest.3e83e2c8a92ce97298a0049ed5205d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19f9c8efd37e469b7e33",
+    "url": "/serenity/static/js/runtime~main.19f9c8ef.js"
+  },
+  {
+    "revision": "62d505a36f72bb0fd003",
+    "url": "/serenity/static/js/main.62d505a3.chunk.js"
+  },
+  {
+    "revision": "1d7a111196713636b719",
+    "url": "/serenity/static/js/1.1d7a1111.chunk.js"
+  },
+  {
+    "revision": "62d505a36f72bb0fd003",
+    "url": "/serenity/static/css/main.4fa49b4a.chunk.css"
+  },
+  {
+    "revision": "1d7a111196713636b719",
+    "url": "/serenity/static/css/1.0c8236da.chunk.css"
+  },
+  {
+    "revision": "90ff51319129c94f1cd4fcf13eda6ba8",
+    "url": "/serenity/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/serenity/precache-manifest.3daa8677238630aff5b627f6a8233a40.js"
+  "/serenity/precache-manifest.3e83e2c8a92ce97298a0049ed5205d73.js"
 );
 
 workbox.clientsClaim();
